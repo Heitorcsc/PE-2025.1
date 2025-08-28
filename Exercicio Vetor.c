8b9e4877f2e7191a85713482fb94f73901e9b9a3
Heitorcsc <heitor.00000856227@unicap.br>
2025-08-28 11:05:12 +0000

    Alteracao final codigo vetor

--- Exercicio Vetor.c
+++ Exercicio Vetor.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 #define TAM 30
-
+ 
 int main() {
     float n1[TAM], n2[TAM], m[TAM], soma = 0, mt;
     int i;
-
-    for (i = 0; i < TAM; i + 1) {
-        printf("Digite a primeira nota %d: ", i+=1);
+ 
+    for (i = 0; i < TAM; i += 1) {
+        printf("Digite a primeira nota do aluno %d: ", i+1);
         scanf("%f", &n1[i]);
-        printf("Digite a segunda nota %d: ", i+=1);
+        printf("Digite a segunda nota do aluno %d: ", i+1);
         scanf("%f", &n2[i]);
-
-        m[i] = (n1[i]*2 + n2[i]*3) / 5;
+ 
+        m[i] = (n1[i]*2 + n2[i]*3) / 5;  
         soma += m[i];
     }
-
+ 
     mt = soma / TAM;
-    printf("Media da turma: %.2f\n", mt);
-
-    printf("Alunos acima da media: ");
-    for (i = 0; i < TAM; i++)
+    printf("Média da turma: %.2f\n", mt);
+ 
+    printf("Alunos acima da média: ");
+    for (i = 0; i < TAM; i += 1) {
         if (m[i] > mt)
             printf("%d ", i+1);
-
-    printf("Medias desses alunos: \n");
-    for (i = 0; i < TAM; i + 1)
+    }
+ 
+    printf("Médias desses alunos: \n");
+    for (i = 0; i < TAM; i += 1) {
         if (m[i] > mt)
             printf("%.2f ", m[i]);
-
+    }
+ 
     return 0;
 }
